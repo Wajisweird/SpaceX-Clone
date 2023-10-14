d9dc7740f231eb322acd7f2832174e0cf6623436
Qazi Wajahat Hussain <wajahathuss17@gmail.com>
2023-10-14 23:56:27 +0500

    commit 1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import First from "./Components/First";
+import Second from "./Components/Second";
+import Third from "./Components/Third";
+import Fourth from "./Components/Fourth";
+import Footer from "./Components/Footer";
+
+export default function App() {
+  return (
+    <div>
+      <>
+        <First />
+        <Second />
+        <Third />
+        <Fourth />
+        <Footer />
+      </>
+    </div>
+  );
+}
